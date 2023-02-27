3e42c2824a7f9f2f91e537ddc9cd826773b2fa5f
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-02-26 23:56:28 -0500

    ok i will fr go to sleep now

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,7 +1,32 @@
 import '../css/style.css'
 import { products } from './products';
 
+const DOMSelectors = {
+    boom:document.querySelector(".boom"),
+    mobs:document.querySelector("#mobs"),
+    money:document.querySelector("#money"),
+    change:document.querySelector("#change"),
+    items:document.querySelector("#items"),
+    customer:document.querySelector("#customer")
+}
+
+// function displayitems(item){
+//     DOMSelectors.items.insertAdjacentHTML("beforeend","
+//    <h1>${item.name}</h1> ");
+// }
 
 
-console.log(products);
+const totalitems = Math.floor(Math.random()*5+1)
 
+console.log(totalitems)
+
+const randomitem = products[Math.floor(Math.random()*products.length)]
+
+console.log(randomitem)
+
+function priceCalc(item,total){
+    const price = item.price*total
+    return price
+
+}
+console.log(priceCalc(randomitem,totalitems))
