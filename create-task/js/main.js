164d59df2eab3593ba0f705edca6270836c86fd2
ericazlin <ericazlin@gmail.com>
2023-02-28 00:39:40 -0500

    yay i am almost done and also i decided on a whim that i will join the fencing team ok bye

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,34 +1,47 @@
-import '../css/style.css'
-import { products } from './products';
+import "../css/style.css";
+import { products } from "./products";
 
 const DOMSelectors = {
-    boom: document.querySelector(".boom"),
-    mobs: document.querySelector("#mobs"),
-    money: document.querySelector("#money"),
-    change: document.querySelector("#change"),
-    items: document.querySelector("#items"),
-    customer: document.querySelector("#customer")
-}
+  boom: document.querySelector(".boom"),
+  mobs: document.querySelector("#mobs"),
+  money: document.querySelector("#money"),
+  change: document.querySelector("#change"),
+  items: document.querySelector("#items"),
+  customer: document.querySelector("#customer"),
+  totalprice: document.querySelector("#totalprice"),
+};
 
 function displayitems(item) {
-    DOMSelectors.items.insertAdjacentHTML("beforeend",
-    `<h2>${totalitems} ${item.name} - $${priceCalc(randomitem, totalitems)}</h2>
-    `);
+  DOMSelectors.items.insertAdjacentHTML(
+    "beforeend",
+    `<h2>${totalitems} ${item.name}</h2>
+    `
+  );
+  DOMSelectors.totalprice.insertAdjacentHTML(
+    "beforeend",
+    `<h2>$${priceCalc(randomitem,totalitems)}</h2>
+    `
+  );
 }
 
+const totalitems = Math.floor(Math.random() * 5 + 1);
+console.log(totalitems);
 
-const totalitems = Math.floor(Math.random() * 5 + 1)
-
-console.log(totalitems)
-
-const randomitem = products[Math.floor(Math.random() * products.length)]
-
-console.log(randomitem)
+const randomitem = products[Math.floor(Math.random() * products.length)];
+console.log(randomitem);
 
 function priceCalc(item, total) {
-    const price = item.price * total
-    return price
+  const price = item.price * total;
+  return price;
 }
-console.log(priceCalc(randomitem, totalitems))
+console.log(priceCalc(randomitem, totalitems));
+displayitems(randomitem);
 
-displayitems(randomitem)
+//random payment amount
+const payment = priceCalc(randomitem, totalitems)
+
+const change = Math.floor(Math.random()*27) ;
+console.log(change)
+
+const given = change+payment
+console.log(given)
