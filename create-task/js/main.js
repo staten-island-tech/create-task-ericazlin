9624f14791feec02a0946a2fb5e1a3a338f6395f
ericazlin <ericazlin@gmail.com>
2023-03-06 02:45:41 -0500

    added iteration

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -18,15 +18,16 @@ const DOMSelectors = {
   image: document.querySelector("#image"),
 };
 //scanned item
-function displayitems(item, totalitems, given) {
+function displayitems(item, item2, totalitems, payment, given) {
   DOMSelectors.numAndItem.insertAdjacentHTML(
     "beforeend",
     `<h2>${totalitems} ${item.name}</h2>
+    <h2>${totalitems} ${item2.name}</h2>
     `
   );
   DOMSelectors.cost.insertAdjacentHTML(
     "beforeend",
-    `<h2 id="costAmount">$${priceCalc(item,totalitems)}</h2>
+    `<h2 id="costAmount">$${payment}</h2>
     `
   );
   DOMSelectors.given.insertAdjacentHTML(
@@ -37,6 +38,7 @@ function displayitems(item, totalitems, given) {
 }
 
 let currentChange = null;
+let currentTotal = null;
 
 function clear() {
   DOMSelectors.numAndItem.innerHTML = ''
@@ -52,22 +54,37 @@ function priceCalc(item, total) {
 function randomize() {
   const totalitems = Math.floor(Math.random() * 5 + 1);
 
-  const randomitem = products[Math.floor(Math.random() * products.length)];
+  const index = Math.floor(Math.random() * products.length)
+
+  const randomitem = products[index];
+  const randomitem2 = products[products.length - index];
+
 
   //random payment amount
-  const payment = priceCalc(randomitem, totalitems);
+  const payment = (priceCalc(randomitem, totalitems) + priceCalc(randomitem2, totalitems));
 
   const change = Math.floor(Math.random() * 27);
-  console.log(change);
 
   const given = change + payment;
 
   currentChange = change;
+  currentTotal = totalitems
 
-  return { randomitem, totalitems, given};
+  return { randomitem, randomitem2, totalitems, payment, given };
 }
 
 function checkChange() {
+
+    let prices = []
+    for (let index = 0; index < products.length; index++) {
+      const item = products[index];
+      let potential = priceCalc(item, currentTotal)
+
+      prices.push(potential)
+    }
+
+    const otherItems = prices.filter(price => price == DOMSelectors.input.value).length
+
   if (DOMSelectors.input.value == currentChange) {
     DOMSelectors.reaction.innerHTML=''
     DOMSelectors.image.innerHTML = ''
@@ -83,7 +100,7 @@ function checkChange() {
     `
     )
   }
-  else {
+  else if(otherItems == 0){
     DOMSelectors.reaction.innerHTML= ''
     DOMSelectors.image.innerHTML = ''
 
@@ -97,16 +114,32 @@ function checkChange() {
     <img src="angrycustomer.png" alt="the customer is angry" />
     `
     )
-}
+  }
+  else{
+    DOMSelectors.reaction.innerHTML= ''
+    DOMSelectors.image.innerHTML = ''
+
+    DOMSelectors.reaction.insertAdjacentHTML("beforeend",
+    `
+    <h1>Incorrect change, the customer is dissatisfied.</h1>
+    <h1>However, you would've been right if it was one of ${otherItems} other items!</h1>
+    `
+    )
+    DOMSelectors.image.insertAdjacentHTML("beforeend", 
+    `
+    <img src="angrycustomer.png" alt="the customer is angry" />
+    `
+    )
+  }
 }
 
 DOMSelectors.btn.addEventListener("click", function () {
   clear();
   checkChange();
-  const {randomitem, totalitems, given} = randomize();
-  displayitems(randomitem, totalitems, given);
+  const {randomitem, randomitem2, totalitems, payment, given} = randomize();
+  displayitems(randomitem, randomitem2, totalitems, payment, given);
   DOMSelectors.input.value = ''
 })
 
-const {randomitem, totalitems, given} = randomize();
-displayitems(randomitem, totalitems, given);
+const {randomitem, randomitem2, totalitems, payment, given} = randomize();
+displayitems(randomitem, randomitem2, totalitems, payment, given);
